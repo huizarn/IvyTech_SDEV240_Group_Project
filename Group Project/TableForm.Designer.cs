8b881a1c7c1c9b3e8580b3376e40a8188e6fd1f3
Naylan Swarens <nswarens@yahoo.com>
2024-09-21 07:24:05 -0400

    Added Group Project folder which contains the project mockup

---
+++ Group Project/TableForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Group_Project
+{
+    partial class TableForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.CategoryLabel = new System.Windows.Forms.Label();
+            this.ItemTextBox = new System.Windows.Forms.TextBox();
+            this.ItemLabel = new System.Windows.Forms.Label();
+            this.UnitCostTextBox = new System.Windows.Forms.TextBox();
+            this.QuantityTextBox = new System.Windows.Forms.TextBox();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.MaterialTextBox = new System.Windows.Forms.TextBox();
+            this.UnitCostLabel = new System.Windows.Forms.Label();
+            this.QuantityLabel = new System.Windows.Forms.Label();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.MaterialLabel = new System.Windows.Forms.Label();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CategoryComboBox
+            // 
+            this.CategoryComboBox.FormattingEnabled = true;
+            this.CategoryComboBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.CategoryComboBox.Location = new System.Drawing.Point(186, 94);
+            this.CategoryComboBox.Name = "CategoryComboBox";
+            this.CategoryComboBox.Size = new System.Drawing.Size(121, 21);
+            this.CategoryComboBox.TabIndex = 0;
+            // 
+            // CategoryLabel
+            // 
+            this.CategoryLabel.AutoSize = true;
+            this.CategoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CategoryLabel.Location = new System.Drawing.Point(215, 75);
+            this.CategoryLabel.Name = "CategoryLabel";
+            this.CategoryLabel.Size = new System.Drawing.Size(62, 16);
+            this.CategoryLabel.TabIndex = 1;
+            this.CategoryLabel.Text = "Category";
+            // 
+            // ItemTextBox
+            // 
+            this.ItemTextBox.Location = new System.Drawing.Point(38, 216);
+            this.ItemTextBox.Name = "ItemTextBox";
+            this.ItemTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ItemTextBox.TabIndex = 2;
+            // 
+            // ItemLabel
+            // 
+            this.ItemLabel.AutoSize = true;
+            this.ItemLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ItemLabel.Location = new System.Drawing.Point(70, 197);
+            this.ItemLabel.Name = "ItemLabel";
+            this.ItemLabel.Size = new System.Drawing.Size(32, 16);
+            this.ItemLabel.TabIndex = 3;
+            this.ItemLabel.Text = "Item";
+            // 
+            // UnitCostTextBox
+            // 
+            this.UnitCostTextBox.Location = new System.Drawing.Point(279, 315);
+            this.UnitCostTextBox.Name = "UnitCostTextBox";
+            this.UnitCostTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UnitCostTextBox.TabIndex = 4;
+            // 
+            // QuantityTextBox
+            // 
+            this.QuantityTextBox.Location = new System.Drawing.Point(111, 315);
+            this.QuantityTextBox.Name = "QuantityTextBox";
+            this.QuantityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.QuantityTextBox.TabIndex = 5;
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(353, 216);
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DescriptionTextBox.TabIndex = 6;
+            // 
+            // MaterialTextBox
+            // 
+            this.MaterialTextBox.Location = new System.Drawing.Point(198, 216);
+            this.MaterialTextBox.Name = "MaterialTextBox";
+            this.MaterialTextBox.Size = new System.Drawing.Size(100, 20);
+            this.MaterialTextBox.TabIndex = 7;
+            // 
+            // UnitCostLabel
+            // 
+            this.UnitCostLabel.AutoSize = true;
+            this.UnitCostLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UnitCostLabel.Location = new System.Drawing.Point(298, 296);
+            this.UnitCostLabel.Name = "UnitCostLabel";
+            this.UnitCostLabel.Size = new System.Drawing.Size(60, 16);
+            this.UnitCostLabel.TabIndex = 8;
+            this.UnitCostLabel.Text = "Unit Cost";
+            // 
+            // QuantityLabel
+            // 
+            this.QuantityLabel.AutoSize = true;
+            this.QuantityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuantityLabel.Location = new System.Drawing.Point(136, 296);
+            this.QuantityLabel.Name = "QuantityLabel";
+            this.QuantityLabel.Size = new System.Drawing.Size(55, 16);
+            this.QuantityLabel.TabIndex = 9;
+            this.QuantityLabel.Text = "Quantity";
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DescriptionLabel.Location = new System.Drawing.Point(367, 197);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(75, 16);
+            this.DescriptionLabel.TabIndex = 10;
+            this.DescriptionLabel.Text = "Description";
+            // 
+            // MaterialLabel
+            // 
+            this.MaterialLabel.AutoSize = true;
+            this.MaterialLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaterialLabel.Location = new System.Drawing.Point(222, 197);
+            this.MaterialLabel.Name = "MaterialLabel";
+            this.MaterialLabel.Size = new System.Drawing.Size(55, 16);
+            this.MaterialLabel.TabIndex = 11;
+            this.MaterialLabel.Text = "Material";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(52, 410);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 12;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(367, 410);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 13;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(202, 410);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 14;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(525, 2);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.Size = new System.Drawing.Size(468, 504);
+            this.DataGridView1.TabIndex = 15;
+            // 
+            // TableForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 506);
+            this.Controls.Add(this.DataGridView1);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.MaterialLabel);
+            this.Controls.Add(this.DescriptionLabel);
+            this.Controls.Add(this.QuantityLabel);
+            this.Controls.Add(this.UnitCostLabel);
+            this.Controls.Add(this.MaterialTextBox);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.QuantityTextBox);
+            this.Controls.Add(this.UnitCostTextBox);
+            this.Controls.Add(this.ItemLabel);
+            this.Controls.Add(this.ItemTextBox);
+            this.Controls.Add(this.CategoryLabel);
+            this.Controls.Add(this.CategoryComboBox);
+            this.Name = "TableForm";
+            this.Text = "Materials List Table";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CategoryComboBox;
+        private System.Windows.Forms.Label CategoryLabel;
+        private System.Windows.Forms.TextBox ItemTextBox;
+        private System.Windows.Forms.Label ItemLabel;
+        private System.Windows.Forms.TextBox UnitCostTextBox;
+        private System.Windows.Forms.TextBox QuantityTextBox;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.TextBox MaterialTextBox;
+        private System.Windows.Forms.Label UnitCostLabel;
+        private System.Windows.Forms.Label QuantityLabel;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.Label MaterialLabel;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button UpdateButton;
+        public System.Windows.Forms.DataGridView DataGridView1;
+    }
+}
